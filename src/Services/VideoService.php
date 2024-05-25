9223fd814696931a5d5b737bd41493b117e7b46e
Homero Raul <h.raulvc@gmail.com>
2024-05-25 09:38:08 -0600

    Update VideoService.php

--- src/Services/VideoService.php
+++ src/Services/VideoService.php
@@ -25,34 +25,38 @@ class VideoService extends AbstractVideoService
         ]);
 
         // Configura el formato de video a X264.
-        $lowBitrate = new X264('aac', 'libx264');
-        $lowBitrate->setKiloBitrate(500);
-
-        $midBitrate = new X264('aac', 'libx264');
-        $midBitrate->setKiloBitrate(1000);
-
-        $highBitrate = new X264('aac', 'libx264');
-        $highBitrate->setKiloBitrate(1500);
+        $lowBitrate = (new X264)->setKiloBitrate(500);
+        $midBitrate = (new X264)->setKiloBitrate(1000);
+        $highBitrate = (new X264)->setKiloBitrate(2000);
+        $superBitrate = (new X264)->setKiloBitrate(3000);
 
         // Exporta el video a HLS.
         $conv = FFMpeg::fromDisk('s3')
             ->open($video->s3_original_path)
             ->exportForHLS()
             ->onProgress(function ($percentage) use ($video) {
-                if($percentage == 100) {
-                    DB::table('videos')->where('id', $video->id)->update([
-                        'status' => 'processing_completed'
-                    ]);
-                }
+                DB::table('videos')->where('id', $video->id)->update([
+                    'progress' => $percentage,
+                    'status' => $percentage == 100 ? 'processing_completed' : 'processing'
+                ]);
             })
             ->setSegmentLength(10) 
             ->setKeyFrameInterval(48) 
             ->withRotatingEncryptionKey(function ($filename, $contents) use ($video) {
                 Storage::disk('s3')->put($video->s3_keys_path . DIRECTORY_SEPARATOR . $filename, $contents);
             })
-            ->addFormat($lowBitrate)
-            ->addFormat($midBitrate)
-            ->addFormat($highBitrate)
+            ->addFormat($lowBitrate, function($media) {
+                $media->scale(480, 360);
+            })
+            ->addFormat($midBitrate, function($media) {
+                $media->scale(960, 720);
+            })
+            ->addFormat($highBitrate, function ($media) {
+                $media->scale(1920, 1080);
+            })
+            ->addFormat($superBitrate, function($media) {
+                $media->scale(2560, 1920);
+            })
             ->save($video->s3_hls_master);
 
         FFMpeg::cleanupTemporaryFiles();
@@ -75,29 +79,22 @@ class VideoService extends AbstractVideoService
             ->fromDisk('s3')
             ->open($path)
             ->setKeyUrlResolver(function ($key) use ($video) {
-
                 return route('videoprocessor.key', [
                     'code' => $video->code,
                     'key' => $key,
                     'guest_token' => request()->guest_token
                 ]);
-
             })
             ->setMediaUrlResolver(function ($mediaFilename) use ($video) {
-
                 $path = $video->s3_hls_path . '/' . $mediaFilename;
-                
                 return Storage::disk('s3')->temporaryUrl($path, now()->addMinutes(5));
-
             })
             ->setPlaylistUrlResolver(function ($playlistFilename) use ($video) {
-
                 return route('videoprocessor.playlist', [
                     'code' => $video->code,
                     'filename' => $playlistFilename,
                     'guest_token' => request()->guest_token
                 ]);
-
             });
     }
 
