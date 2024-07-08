c9470418fa02d008b2ca372b17c874ef42809b3b
Homero Raul <h.raulvc@gmail.com>
2024-07-08 00:52:35 -0600

    Update ProcessVideoJob.php

--- src/Jobs/ProcessVideoJob.php
+++ src/Jobs/ProcessVideoJob.php
@@ -9,26 +9,36 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Innoboxrr\VideoProcessor\Services\VideoService;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
-class ProcessVideoJob implements ShouldQueue
+class ProcessVideoJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $video;
+    protected $videoId;
+
+    public $uniqueFor = 3600;
 
     public function __construct($videoId)
     {
-        $this->video = Video::findOrFail($videoId);
+        $this->videoId = $videoId;
+    }
+
+    public function uniqueId(): string
+    {
+        return $this->videoId;
     }
 
     public function handle(VideoService $videoService)
     {
+        $video = Video::findOrFail($this->videoId);
 
-        $this->video->update([
+        $video->update([
             'cloud' => 'aws',
             'status' => 'queue_for_processing',
+            'progress' => 0,
         ]);
 
-        $videoService->processVideo($this->video->id);
+        $videoService->processVideo($video->id);
     }
 }
