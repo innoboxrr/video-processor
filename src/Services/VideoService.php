e941fe77dad7ae9592dd7e118b6317ef60875ef8
Homero Raul <h.raulvc@gmail.com>
2024-09-24 22:06:34 -0600

    Update VideoService.php

--- src/Services/VideoService.php
+++ src/Services/VideoService.php
@@ -102,7 +102,8 @@ class VideoService extends AbstractVideoService
             })
             ->setMediaUrlResolver(function ($mediaFilename) use ($video) {
                 $path = $video->s3_hls_path . '/' . $mediaFilename;
-                return Storage::disk('s3')->temporaryUrl($path, now()->addMinutes(value: 240));
+                return Storage::disk('s3')
+                    ->temporaryUrl($path, now()->addMinutes(value: 240));
             })
             ->setPlaylistUrlResolver(function ($playlistFilename) use ($video) {
                 return route('videoprocessor.playlist', [
