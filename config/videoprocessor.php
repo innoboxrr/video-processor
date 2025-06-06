0d37ab824c26ee6eec3b31aed6adf450a43dd260
Homero Raul <h.raulvc@gmail.com>
2025-06-06 09:40:02 -0600

    Update ffmpeg and ffprobe paths in videoprocessor configuration

--- config/videoprocessor.php
+++ config/videoprocessor.php
@@ -43,37 +43,22 @@ return [
     ],
 	
     'enabled_formats' => ['low', 'mid', 'high'],
-
-	'ffmpeg_path' => env('FFMPEG_PATH', 'C:/xampp/htdocs/github/innoboxrr/video-processor/bin/ffmpeg/ffmpeg.exe'),
-
-	'ffprobe_path' => env('FPROBE_PATH', 'C:/xampp/htdocs/github/innoboxrr/video-processor/bin/ffmpeg/ffprobe.exe'),
-
+	'ffmpeg_path' => env('FFMPEG_PATH', '/usr/bin/ffmpeg'),
+	'ffprobe_path' => env('FPROBE_PATH', '/usr/bin/ffprobe'),
     'ffmpeg_threads' => env('FFMPEG_THREADS', 12),
-
 	's3_url' => env('S3_URL', 'https://innoboxrr.s3.amazonaws.com'),
-
     'cloudfront_url' => env('CLOUDFRONT_URL', 'd1st6n2eacne1j.cloudfront.net'),
-
     'video_path' => env('VIDEO_PATH', 'videos'),
-
 	'guest_token_secret' => env('VIDEOPROCESSOR_GUEST_TOKEN_SECRET', 'secret'),
 
     // VTT
-
     'vtt' => [
-
         'auto-generate' => env('VTT_AUTO_GENERATE', 1),
-
     ],
 
 	// Player settings
-
 	'video_watermark' => env('VIDEO_WATERMARK'),
-
 	'video_icon' => env('VIDEO_ICON'),
-
 	'video_allow_embed' => env('VIDEO_ALLOW_EMBED', 0),
-
 	'video_show_watermark' => env('VIDEO_SHOW_WATERMARK', 0),
-	
 ];
